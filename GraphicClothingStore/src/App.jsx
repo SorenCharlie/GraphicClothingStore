fdc144ff7f9b8c6fb04370e05b2a5a36658d58b6
KobESB <robertskobie@gmail.com>
2024-10-05 11:40:53 -0700

    fixed component typos/comments

--- GraphicClothingStore/src/App.jsx
+++ GraphicClothingStore/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import GraphicClothingShop from './pages/clothing';
 import './App.css'
 
 function App() {
     return (
         <div className="App">
-          <ClothingItems />
+          <GraphicClothingShop />
         </div>
       );
     }
