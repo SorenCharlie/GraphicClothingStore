842c6e1cc5017af89c1f23a8e62858a8963e7ab1
SorenCharlie <sorenr2209@gmail.com>
2024-10-15 18:41:43 -0700

    login first

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import React from 'react';
 import ImageSelector from './pages/graphicImages';
 import ClothingSelector from './pages/clothing';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 import Accordion from '@mui/material/Accordion';
 import AccordionActions from '@mui/material/AccordionActions';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -97,31 +99,3 @@ function App() {
 }
 
 export default App
-
-// const [count, setCount] = useState(0)
-
-// return (
-//   <>
-//     <div>
-//       <a href="https://vitejs.dev" target="_blank">
-//         <img src={viteLogo} className="logo" alt="Vite logo" />
-//       </a>
-//       <a href="https://react.dev" target="_blank">
-//         <img src={reactLogo} className="logo react" alt="React logo" />
-//       </a>
-//     </div>
-//     <h1>Vite + React</h1>
-//     <div className="card">
-//       <button onClick={() => setCount((count) => count + 1)}>
-//         count is {count}
-//       </button>
-//       <p>
-//         Edit <code>src/App.jsx</code> and save to test HMR
-//       </p>
-//     </div>
-//     <p className="read-the-docs">
-//       Click on the Vite and React logos to learn more
-//     </p>
-//   </>
-// )
-
